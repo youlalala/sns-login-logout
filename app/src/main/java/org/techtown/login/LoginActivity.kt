df341bd16bd3e530d90ab6c26d3c14edd6feab26
youla <reyoula@gmail.com>
2022-01-13 19:22:11 +0900

    2. add logout

--- app/src/main/java/org/techtown/login/LoginActivity.kt
+++ app/src/main/java/org/techtown/login/LoginActivity.kt
@@ -1,31 +1,108 @@
 package org.techtown.login
 
+import android.app.Activity
+import android.app.PendingIntent.getActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
+import org.techtown.login.databinding.ActivityLoginBinding
 
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
-        // 사용자 정보 요청 (기본)
-        UserApiClient.instance.me { user, error ->
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
-                Log.d("sss", "사용자 정보 요청 실패", error)
+                //Login Fail
+                Log.d("sss", "로그인 실패", error)
+            } else if (token != null) {
+                //Login Success
+                Log.d("sss", "로그인 성공", error)
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.putExtra("login", "kakao")
+                startActivity(intent)
             }
-            else if (user != null) {
-                Log.d(
-                    "sss", "사용자 정보 요청 성공" +
-                            "\n회원번호: ${user.id}" +
-                            "\n이메일: ${user.kakaoAccount?.email}" +
-                            "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
-                            "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}"
-                )
+        }
+
+
+        binding.kakaoLoginBtn.setOnClickListener {
+            UserApiClient.instance.run {
+                if (isKakaoTalkLoginAvailable(this@LoginActivity)) {
+                    loginWithKakaoTalk(this@LoginActivity, callback = callback)
+                } else {
+                    loginWithKakaoAccount(this@LoginActivity, callback = callback)
+                }
             }
+        }
+
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.server_client_id))
+                .requestEmail()
+                .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        binding.googleLoginBtn.setOnClickListener {
+            val intent = mGoogleSignInClient.signInIntent
+            startActivityForResult(intent, 1)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 1) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+
+
+            handleSignInResult(task)
+
+
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            val email = account.email
+            Log.d("sss", email.toString())
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account)
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("login", "google")
+            //intent.putExtra("email",email)
+            startActivity(intent)
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.i("SSS", "signInResult:failed code=" + e.statusCode)
+            //updateUI(null)
         }
 
 
--- app/src/main/java/org/techtown/login/MainActivity.kt
+++ app/src/main/java/org/techtown/login/MainActivity.kt
@@ -4,10 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
@@ -15,105 +18,78 @@ import org.techtown.login.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mGoogleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            if (error != null) {
-                //Login Fail
-                Log.d("sss", "로그인 실패", error)
-            } else if (token != null) {
-                //Login Success
-                Log.d("sss", "로그인 성공", error)
-                val intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
-            }
-        }
-
-
-        binding.kakaoLoginBtn.setOnClickListener {
-            UserApiClient.instance.run {
-                if (isKakaoTalkLoginAvailable(this@MainActivity)) {
-                    loginWithKakaoTalk(this@MainActivity, callback = callback)
-                } else {
-                    loginWithKakaoAccount(this@MainActivity, callback = callback)
-                }
-            }
-        }
-
-
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.server_client_id))
-            .requestEmail()
-            .build()
+                .requestIdToken(getString(R.string.server_client_id))
+                .requestEmail()
+                .build()
 
         // Build a GoogleSignInClient with the options specified by gso.
-        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        binding.googleLoginBtn.setOnClickListener{
-            val intent = mGoogleSignInClient.signInIntent
-            startActivityForResult(intent, 1)
+        var intent=intent
+        if (intent.getStringExtra("login") == "kakao"){
+            // 사용자 정보 요청 (기본)
+            UserApiClient.instance.me { user, error ->
+                if (error != null) {
+                    Log.d("sss", "사용자 정보 요청 실패", error)
+                }
+                else if (user != null) {
+                    Log.d(
+                            "sss", "사용자 정보 요청 성공" +
+                            "\n회원번호: ${user.id}" +
+                            "\n이메일: ${user.kakaoAccount?.email}" +
+                            "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                            "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}"
+                    )
+                    binding.email.text = user.kakaoAccount?.email
+
+                }
+            }
+        }else if(intent.getStringExtra("login") == "google"){
+            val acct = GoogleSignIn.getLastSignedInAccount(this)
+            binding.email.text = acct?.email
         }
 
+        binding.logoutBtn.setOnClickListener {
+            signOut()
+        }
+
+
     }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, OnCompleteListener<Void?> {
+                    val intent= Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                })
+    }
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == 1) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+    override fun onStart() {
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
 
-//            val intent = Intent(this, LoginActivity::class.java)
-//            startActivity(intent)
-
-            handleSignInResult(task)
-
-
-
-
+        if(account!=null) {
+            Log.d("sss", "로그인 기록 있음")
+        }else{
+            Log.d("sss", "로그인 기록 없음")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
-
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
-
-            val email = account.email
-            Log.d("sss", email.toString())
-
-
-
-            // Signed in successfully, show authenticated UI.
-            //updateUI(account)
-        } catch (e: ApiException) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.i("SSS", "signInResult:failed code=" + e.statusCode)
-            //updateUI(null)
-        }
-    }
-
-//    override fun onStart() {
-//        // Check for existing Google Sign In account, if the user is already signed in
-//        // the GoogleSignInAccount will be non-null.
-//        super.onStart()
-//        val account = GoogleSignIn.getLastSignedInAccount(this)
-//
-//        if(account!=null) {
-//            Log.d("sss", "로그인 기록 있음")
-//            val intent = Intent(this, LoginActivity::class.java)
-//            startActivity(intent)
-//        }else{
-//            Log.d("sss", "로그인 기록 없음")
-//        }
-//    }
 }
+
+
+
 
 
